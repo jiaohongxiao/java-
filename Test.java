96021e8b8b6d2b1ee8bb630bdfc444f750a689fe
jiaohongxiao <72740540+jiaohongxiao@users.noreply.github.com>
2020-10-29 11:13:08 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+package jhx;
+
+public class Test {
+	public static void main(String[] args){
+		CPU cpu=new CPU();
+		cpu.setSpeed(2200);
+		cpu.setBrand("´÷¶û");
+		HardDisk disk=new HardDisk();
+		disk.setAmount(200);
+		disk.setPrice(3800);
+		PC pc=new PC();
+		pc.setCPU(cpu);
+		pc.setHardDisk(disk);
+		pc.show();
+	}
+
+}
