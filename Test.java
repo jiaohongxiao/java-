756c49ae54d9f33c6b5545d852948b299b5d79dd
jiaohongxiao <72740540+jiaohongxiao@users.noreply.github.com>
2020-10-29 11:16:27 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,9 @@
-package jhx;
-
+//交第三次实验不小心删除，再次上传
 public class Test {
 	public static void main(String[] args){
 		CPU cpu=new CPU();
 		cpu.setSpeed(2200);
-		cpu.setBrand("����");
+		cpu.setBrand("´÷¶û");
 		HardDisk disk=new HardDisk();
 		disk.setAmount(200);
 		disk.setPrice(3800);
